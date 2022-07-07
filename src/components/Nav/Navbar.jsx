a74ff935f667cf9f49ff49333b8cfacb8aa979b8
livvstrongg <livyhawk@me.com>
2022-07-07 12:43:26 -0500

    Working on navbar

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import './navbar.scss';
+import "./navbar.scss";
 
-function Navbar() {
+function Navbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='navbar'>Navbar</div>
-  )
+    <div className={"navbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <span>+44 924 12 74</span>
+          </div>
+          <div className="itemContainer">
+            <span>safak@genius.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
