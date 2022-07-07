37f520a9b522ebf7accef9f3de6d06d55afc8e08
livvstrongg <livyhawk@me.com>
2022-07-07 13:50:15 -0500

    Making navbar active

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import Projects from "./components/Projects/Projects";
 import References from "./components/References/References";
 import Contact from "./components/Contact/Contact";
+import { useState} from "react";
 import './app.scss';
 
+
 function App() {
+  const [menuOpen, setMenuOpen ] = useState(true)
   return (
     <div className="app">
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
