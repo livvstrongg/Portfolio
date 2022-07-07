2814849b67cb4471a851b5fa24981a5f2c712579
livvstrongg <livyhawk@me.com>
2022-07-06 19:43:32 -0500

    Spacing out sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/Nav/Navbar";
-import Intro from "./components/intro/intro";
-import Portfolio from "./components/portfolio/Portfolio";
+import Intro from "./components/Intro/Intro";
+import Portfolio from "./components/Portfolio/Portfolio";
 import Projects from "./components/Projects/Projects";
 import References from "./components/References/References";
 import Contact from "./components/Contact/Contact";
+import './app.scss';
 
 function App() {
   return (
