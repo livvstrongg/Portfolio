f35bc28ed131bed3efb8e82b3ae03ed61dd1a0b0
livvstrongg <livyhawk@me.com>
2022-07-13 13:31:50 -0500

    Intro

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './intro.scss';
+import "./intro.scss";
+import { useEffect, useRef } from "react";
 
-function Intro() {
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+  }, []);
+
   return (
-    <div className='intro' id='intro'>intro</div>
-  )
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="https://i.imgur.com/YgK70sp.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Olivia Grass</h1>
+          <h3>
+            Web Developer & Graphic Designer<span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
-
-export default Intro
