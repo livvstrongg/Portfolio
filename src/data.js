dc1ee851ceced5413a5bc648d17730d3c88be769
livvstrongg <livyhawk@me.com>
2022-07-13 14:00:36 -0500

    Added data component

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const featuredPortfolio = [
+    {
+      id: 1,
+      title: "Social Media App",
+      img:
+        "https://polar-bastion-57023.herokuapp.com/posts",
+    },
+    {
+      id: 2,
+      title: "Game of War",
+      img:
+        "https://livvstrongg.github.io/Game-of-War/",
+    },
+    {
+      id: 3,
+      title: "Recipe Search API",
+      img:
+        "https://lesemotifsanonymes.herokuapp.com/",
+    },
+    {
+      id: 4,
+      title: "E-commerce App",
+      img:
+        "https://tranquil-begonia-e33eb9.netlify.app/",
+    },
+
+  ];
