d270d27dabee51b5758a464f57a1a45d19d56870
livvstrongg <livyhawk@me.com>
2022-07-13 12:37:37 -0500

    Menu update

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
+import "./menu.scss";
 
-function Menu() {
+export default function Menu({ menuOpen, setMenuOpen }) {
   return (
-    <div className='menu'>
-        <ul>
-            <li>
-            <a href="#intro">Home</a>
-            </li>
-            <li>
-            <a href="#portfolio">Portfolio</a>
-            </li>
-            <li>
-            <a href="#projects">Projects</a>
-            </li>
-            <li>
-            <a href="#references">References</a>
-            </li>
-            <li>
-            <a href="#contact">Contact</a>
-            </li>
-        </ul>
-
+    <div className={"menu "+(menuOpen && "active")}>
+      <ul>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#intro">Home</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#projects">Projects</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#references">References</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
-
-export default Menu
