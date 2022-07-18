be32eb7ddbe8e9d7f383fb986d950d3803481b8a
livvstrongg <livyhawk@me.com>
2022-07-18 16:59:25 -0500

    Added recipe app photos

--- src/data.js
+++ src/data.js
@@ -65,37 +65,25 @@ export const featuredPortfolio = [
       id: 1,
       title: "Mobile Social Media App",
       img:
-        "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
+        "https://i.imgur.com/7e5HwgA.png",
     },
     {
       id: 2,
       title: "Mobile Rampa UI Design",
       img:
-        "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
+        "https://i.imgur.com/8AvD6hi.png",
     },
     {
       id: 3,
       title: "Mobile E-commerce Design",
       img:
-        "https://cdn.dribbble.com/users/1998175/screenshots/15459384/media/48ac2b43ebe81ba0866afea1383cc939.png?compress=1&resize=1200x900",
+        "https://i.imgur.com/5Uu54L1.png",
     },
     {
       id: 4,
       title: "Mobile Relax App",
       img:
-        "https://cdn.dribbble.com/users/2083704/screenshots/15468619/media/cd958306c7a772449e1ac23bd65ce506.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 5,
-      title: "Mobile Hero Design",
-      img:
-        "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 6,
-      title: "Mobile Banking App",
-      img:
-        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+        "https://i.imgur.com/gQZpI1t.png",
     },
   ];
   
