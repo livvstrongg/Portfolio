2db895ef3f88822e6afc10165ae858f751b253fb
livvstrongg <livyhawk@me.com>
2022-07-18 17:38:22 -0500

    Added all sites, need to add links to live site and github next

--- src/data.js
+++ src/data.js
@@ -81,25 +81,26 @@ export const skills = [
   export const recipeApp = [
     {
       id: 1,
-      title: "Mobile Social Media App",
+      website: "https://lesemotifsanonymes.herokuapp.com/",
+      title: "",
       img:
         "https://i.imgur.com/7e5HwgA.png",
     },
     {
       id: 2,
-      title: "Mobile Rampa UI Design",
+      title: "",
       img:
         "https://i.imgur.com/8AvD6hi.png",
     },
     {
       id: 3,
-      title: "Mobile E-commerce Design",
+      title: "",
       img:
         "https://i.imgur.com/5Uu54L1.png",
     },
     {
       id: 4,
-      title: "Mobile Relax App",
+      title: "",
       img:
         "https://i.imgur.com/gQZpI1t.png",
     },
@@ -108,25 +109,25 @@ export const skills = [
   export const socialMediaApp = [
     {
       id: 1,
-      title: "Design Social Media App",
+      title: "",
       img:
         "https://i.imgur.com/T4qWFN2.png",
     },
     {
       id: 2,
-      title: "Design Rampa UI Design",
+      title: "",
       img:
         "https://i.imgur.com/GXtEqsB.png",
     },
     {
       id: 3,
-      title: "Design E-commerce Web Design",
+      title: "",
       img:
         "https://i.imgur.com/7m4WpN2.png",
     },
     {
       id: 4,
-      title: "Design Relax Mobile App",
+      title: "",
       img:
         "https://i.imgur.com/ZbnHLFR.png",
     },
@@ -135,25 +136,25 @@ export const skills = [
   export const gameApp = [
     {
       id: 1,
-      title: "Content Social Media App",
+      title: "",
       img:
         "https://user-images.githubusercontent.com/101207297/163058390-f65cbbbf-7031-4dea-aecf-58eafeca2a6d.png",
     },
     {
       id: 2,
-      title: "Content Rampa UI Design",
+      title: "",
       img:
         "https://user-images.githubusercontent.com/101207297/163058400-539338ec-31bb-4bc7-9705-1c729adeba1f.png",
     },
     {
       id: 3,
-      title: "Content E-commerce Web Design",
+      title: "",
       img:
         "https://user-images.githubusercontent.com/101207297/163058406-de68311d-ede3-41a7-a075-c434204fcb78.png",
     },
     {
       id: 4,
-      title: "Content Relax Mobile App",
+      title: "",
       img:
         "https://user-images.githubusercontent.com/101207297/163058414-d7e0aa2a-6f39-45d6-8760-22d00cc9e3ca.png",
     },
