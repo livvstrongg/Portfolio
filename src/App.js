074262f20202168bee5a6775bfe8df0aeb00c919
livvstrongg <livyhawk@me.com>
2022-07-22 14:35:02 -0500

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/Nav/Navbar";
 import Intro from "./components/Intro/Intro";
-import Projects from "./components/Projects/Projects";
 import Portfolio from "./components/Portfolio/Portfolio";
+import Projects from "./components/Projects/Projects";
 import References from "./components/References/References";
 import Contact from "./components/Contact/Contact";
 import { useState} from "react";
@@ -15,8 +15,8 @@ function App() {
       <Navbar />
       <div className="sections">
         <Intro />
-        <Projects />
         <Portfolio />
+        <Projects />
         <References />
         <Contact />
       </div>
