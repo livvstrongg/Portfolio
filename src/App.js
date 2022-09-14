a26da1c2d1c833d39ecfc63b7097eac4828cd8a7
livvstrongg <livyhawk@me.com>
2022-09-14 11:46:16 -0500

    Reconfig

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
+      <About /> 
+ 
     </main>
   );
 }
-
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">OG</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">References</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  );
+}
