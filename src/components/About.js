d8f6e84edb989c499714decc8bc1e0336fd3a829
livvstrongg <livyhawk@me.com>
2022-09-14 12:01:15 -0500

    About page info

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,35 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="about">
-      <h1>Olivia Grass</h1>
-    </div>
-
+    <section id="about">
+      <div>
+        <div>
+          <h1>
+            Hey, I'm Olivia.
+            <br/>Welcome to my Portfolio.
+          </h1>
+          <p>
+            
+          </p>
+          <div>
+            <a
+              href="#contact">
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              >
+              View my past work.
+            </a>
+          </div>
+        </div>
+        <div>
+          <img
+            alt="hero"
+            src="https://i.imgur.com/pF6nHXr.jpeg"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
