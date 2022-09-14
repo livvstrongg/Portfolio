0d4b9d669a8800a7e1390d1f7015749bc90adb5d
livvstrongg <livyhawk@me.com>
2022-09-14 12:36:47 -0500

    Adding projects

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
-      <About /> 
+      <About />
+      <Projects /> 
  
     </main>
   );
